bb97929d0cdcae3632ebe88646832c3600b1b80c
No1dem <demantos3@gmail.com>
2024-02-07 14:01:14 +0100

    Primo commit OO

---
+++ Object Orientation/UninaSocialGroup/src/ClassiDAO/Gruppo.java
@@ -0,0 +1,65 @@
+package ClassiDAO;
+
+public class Gruppo {
+	private int idGruppo;
+	private String nomeGruppo;
+	private String tagGruppo;
+	private String descrizioneGruppo;
+	private int numeroIscritti;
+	
+	// Costruttore
+	
+	public Gruppo(int IdG, String NomeG, String TagG, String DescrG, int NumI) {
+		super();
+		idGruppo = IdG;
+		nomeGruppo = NomeG;
+		tagGruppo = TagG;
+		descrizioneGruppo = DescrG;
+		numeroIscritti = NumI;
+	}
+	
+	// Getters e Setters
+
+	public int getIdGruppo() {
+		return idGruppo;
+	}
+
+	public void setIdGruppo(int idGruppo) {
+		this.idGruppo = idGruppo;
+	}
+
+	public String getNomeGruppo() {
+		return nomeGruppo;
+	}
+
+	public void setNomeGruppo(String nomeGruppo) {
+		this.nomeGruppo = nomeGruppo;
+	}
+
+	public String getTagGruppo() {
+		return tagGruppo;
+	}
+
+	public void setTagGruppo(String tagGruppo) {
+		this.tagGruppo = tagGruppo;
+	}
+
+	public String getDescrizioneGruppo() {
+		return descrizioneGruppo;
+	}
+
+	public void setDescrizioneGruppo(String descrizioneGruppo) {
+		this.descrizioneGruppo = descrizioneGruppo;
+	}
+
+	public int getNumeroIscritti() {
+		return numeroIscritti;
+	}
+
+	public void setNumeroIscritti(int numeroIscritti) {
+		this.numeroIscritti = numeroIscritti;
+	}
+	
+
+		
+}
---
+++ Object Orientation/UninaSocialGroup/src/DataBaseConnection/ConnectDB.java
@@ -0,0 +1,25 @@
+package DataBaseConnection;
+import java.sql.*;
+
+public class ConnectDB{	
+	
+	private static Connection myConn;
+	
+	static {
+		try {     //Inizializzazione delle connessione
+			String url = "jdbc:postgresql://localhost:5432/DBUninaSG";
+		    String username = "postgres";
+		    String password = "postgres";
+			Class.forName("org.postgresql.Driver");
+			myConn=DriverManager.getConnection(url,username,password);
+		}
+		catch(ClassNotFoundException | SQLException e){ 
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		return myConn;
+	}
+	
+}
