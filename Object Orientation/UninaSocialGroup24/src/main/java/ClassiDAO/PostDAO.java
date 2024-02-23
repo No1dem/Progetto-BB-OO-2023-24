8d0a5b1e51c56f8e309280f725ba176acb12b755
No1dem <demantos3@gmail.com>
2024-02-23 20:15:34 +0100

    Connessione PostDAO

--- Object Orientation/UninaSocialGroup24/src/main/java/ClassiDAO/PostDAO.java
+++ Object Orientation/UninaSocialGroup24/src/main/java/ClassiDAO/PostDAO.java
@@ -1,5 +1,36 @@
 package ClassiDAO;
 
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 public class PostDAO {
+	private Connection connessioneDB; 
+	private LinkedList<Post> listaPost;
 	
+	public void listaPostDao(Connection conn) {
+		String query="SELECT * FROM Post";
+		listaPost = new LinkedList<Post>();
+		try(Statement stmt=conn.createStatement()){
+			ResultSet res=stmt.executeQuery(query);
+			while(res.next()) {
+				// Conversione java.sql.Date in LocalDate
+	            java.sql.Date sqlDate = res.getDate("dataPubblicazione");
+	            LocalDate dataPubblicazione = sqlDate.toLocalDate();
+	            // Conversione java.sql.Time in LocalTime
+	            java.sql.Time sqlTime = res.getTime("oraPubblicazione");
+	            LocalTime oraPubblicazione = sqlTime.toLocalTime();
+				listaPost.add(new Post(res.getInt("idPost"),res.getString("testo"),res.getString("urlImmagine"),
+						dataPubblicazione,oraPubblicazione,res.getInt("NumeroLike"),res.getInt("NumeroCommenti")));  				
+			}
+			connessioneDB=conn;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
