1c3ec1a7cc8b9ef12821f2b2be9b590507c12df8
Davidcat1118 <dav.gatta@studenti.unina.it>
2024-02-23 19:55:08 +0100

    Gruppo DAO

--- Object Orientation/UninaSocialGroup24/src/main/java/ClassiDAO/GruppoDAO.java
+++ Object Orientation/UninaSocialGroup24/src/main/java/ClassiDAO/GruppoDAO.java
@@ -1,5 +1,30 @@
 package ClassiDAO;
 
-public class GruppoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 
+
+public class GruppoDAO {
+	private Connection connessioneDB;
+	private LinkedList<Gruppo> listaGruppi;
+		
+	public void listaGruppiDao(Connection conn) {
+			String query="SELECT * FROM Gruppo";
+			listaGruppi = new LinkedList<Gruppo>();
+			try(Statement stmt=conn.createStatement()){
+				ResultSet res=stmt.executeQuery(query);
+				while(res.next()) {
+					listaGruppi.add(new Gruppo (res.getInt("IdGruppo"),res.getString("nomeGruppo"),res.getString("tagGruppo"),res.getString("descrizioneGruppo"),
+							                    res.getInt("numeroIscritti")));
+                }
+			    connessioneDB=conn;
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}	
+	}
 }
